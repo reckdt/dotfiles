1a29f3bd72ee7c2f1ccce85c077784f6c0efd64b
rhebase <>
2021-05-17 12:06:27 -0500

    add tmux config

--- setup.sh
+++ setup.sh
@@ -8,5 +8,6 @@ chmod +x $PWD/scripts/*
 ln -sf $PWD/scripts/* $HOME/bin
 ln -sf $PWD/vimrc $HOME/.vimrc
 ln -sf $PWD/aliases $HOME/.aliases
+ln -sf $PWD/tmux.conf $HOME/.tmux.conf
 
 echo "Complete."
